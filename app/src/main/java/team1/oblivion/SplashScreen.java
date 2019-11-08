da206ed12ffe40aabaaf6374d7c42244a4928e8c
leonoronsantos <leosud099@gmail.com>
2019-11-08 16:56:36 -0700

    Splash screen working / Action bar hidden / Missing correct image

--- app/src/main/java/team1/oblivion/SplashScreen.java
+++ app/src/main/java/team1/oblivion/SplashScreen.java
@@ -1,9 +1,11 @@
 package team1.oblivion;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,12 +14,21 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Thread thread = new Thread(){
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+       ActionBar actionBar = getSupportActionBar();
+       actionBar.hide();
+
+        Thread thread = new Thread() {
             @Override
             public void run() {
                 try {
-                    // 3 seconds delay
-                    sleep(3000);
+                    // 4 seconds delay
+                    sleep(4000);
                     // Send it to Main Activity
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
