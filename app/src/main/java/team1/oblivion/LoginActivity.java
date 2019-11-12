4eb55a35c93dd92ecb9736d7e09b5fcc93075a53
leonoronsantos <leosud099@gmail.com>
2019-11-11 22:09:14 -0700

    Login Activity design implemented

--- app/src/main/java/team1/oblivion/LoginActivity.java
+++ app/src/main/java/team1/oblivion/LoginActivity.java
@@ -1,8 +1,10 @@
 package team1.oblivion;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
     }
 }
