23e1422dbc8b0888c2534e681146564f62e42a5f
guibyiu <guimn@byui.edu>
2019-11-13 21:18:44 -0700

    Meeting, Task, Report, and Calendar fragments created and working - Fixed bottom line navigation bar color

--- app/src/main/java/team1/oblivion/MainActivity.java
+++ app/src/main/java/team1/oblivion/MainActivity.java
@@ -1,11 +1,16 @@
 package team1.oblivion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +18,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBar actionBar =getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navi);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.home_page:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.task_page:
+                            selectedFragment = new TaskFragment();
+                            break;
+                        case R.id.report_page:
+                            selectedFragment = new ReportFragment();
+                            break;
+                        case R.id.calendar_page:
+                            selectedFragment = new CalendarFragment();
+                            break;
+
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
