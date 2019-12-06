2a9935cb8060a5800d59b0e8369528ce10a24bd2
guibyiu <guimn@byui.edu>
2019-12-05 22:12:15 -0700

    Trying to make the a better meeting id on firebase -- Working on the data display from firebase.

--- app/src/main/java/team1/oblivion/HomeFragment.java
+++ app/src/main/java/team1/oblivion/HomeFragment.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +20,10 @@ import androidx.fragment.app.Fragment;
 public class HomeFragment extends Fragment {
 
     private View rootView;
+//    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+//    final DatabaseReference meeting_user = database.getReference("Meetings");
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,6 +38,22 @@ public class HomeFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+//        meeting_user.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+//                    String title = ds.child("DateTimeTitle").child("title").getValue().toString();
+//                    String date = ds.child("DateTimeTitle").child("date").getValue().toString();
+//                    String time = ds.child("DateTimeTitle").child("time").getValue().toString();
+//                }
+//            }
+
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+//            }
+//        });
 
         return rootView;
     }
